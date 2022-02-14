cc6bdcc120953884e81a14cbe7a6aa7e24a3813e
Pablo Maldonado <p.maldonado@colostate.edu>
2022-02-13 17:38:20 -0700

    refernce script

--- Reference_script.R
+++ Reference_script.R
@@ -1,5 +1,21 @@
-# Section 2
+# Section 1.6
+total_cases <- coronavirus %>%
+  filter(type != "recovery") %>% # "!=" (not equal to)
+  group_by(type) %>%
+  summarise(cases = sum(cases)) %>%
+  mutate(type = factor(type, levels = c("confirmed", "death"))) 
+# Factors are used to represent categorical data which cannot be done with a character vector
 
+       # Simplified
+total_cases <- coronavirus %>%
+  filter(type != "recovery") %>% # "!=" (not equal to)
+  group_by(type) %>%
+  summarise(cases = sum(cases)) 
+
+
+
+
+# Section 2
 coronavirus %>%
   filter(type != "recovery") %>%
   group_by(type, continent_name) %>%
@@ -8,18 +24,28 @@ coronavirus %>%
   pivot_wider(names_from = type, values_from = cases) %>%
   mutate(death_rate = death / confirmed) %>%
   filter(!is.na(continent_name)) %>%
-  arrange(-death_rate) %>%
+  arrange(-confirmed) %>%
   datatable(rownames = FALSE,
             colnames = c("Continent", "Confrimed Cases", "Death Cases","Death Rate %")) %>%
   formatPercentage("death_rate", 2)
 
-# Simplified
+       # Simplified
 coronavirus %>%
   filter(type != "recovery") %>%
   group_by(type, country) %>%
   summarise(cases = sum(cases)) %>%
+  mutate(type = factor(type, levels = c("confirmed", "death"))) %>%
   pivot_wider(names_from = type, values_from = cases) %>%
   mutate(death_rate = death / confirmed) %>%
   datatable(rownames = FALSE,
             colnames = c("Country", "Confrimed Cases", "Death Cases","Death Rate %")) %>%
   formatPercentage("death_rate", 0)
+
+
+
+
+
+
+
+
+
